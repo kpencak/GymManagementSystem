be02a8756c7fd1bb0b79d938777c063bc0a44811
Binek, Asia <Joanna.Binek@pega.com>
2020-06-04 23:55:57 +0200

    Icons are finally visible

--- gym_manager/config/application.rb
+++ gym_manager/config/application.rb
@@ -11,6 +11,7 @@ module GymManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.active_record.schema_format = :ruby
+    config.assets.enabled = truec
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
